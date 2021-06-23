231635e1fabff80399e35f428a439b0848e81091
Den Glazunov <glazunovden@yandex.ru>
2021-06-23 18:51:29 +0300

    сделал активные и не активные состояния форм

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import {getOffers} from './data.js';
 import {genereteDomElements} from './dom-generate.js';
+import {disabledForm, unDisabledForm} from './state-form.js';
 
 const SIMILAR_OFFER_COUNT = 10;
 genereteDomElements(getOffers(SIMILAR_OFFER_COUNT));
+disabledForm();
+setTimeout(unDisabledForm, 2000);
